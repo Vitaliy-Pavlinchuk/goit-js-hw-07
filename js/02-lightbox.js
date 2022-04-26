baddc27344a80b7ec6352cf50ebc95e2274464b3
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-04-26 17:53:17 +0300

    finish-2-task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsContainer = document.querySelector(".gallery");
+const cardsMarcup = createColorCardsMarkup(galleryItems);
+galleryItemsContainer.insertAdjacentHTML("beforeend", cardsMarcup);
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+</a>
+</div>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
