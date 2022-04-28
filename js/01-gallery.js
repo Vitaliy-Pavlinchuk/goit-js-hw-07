58c95548f1aa6e099824bbe6ad8822a2f915eaf1
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-04-28 16:01:19 +0300

    add-escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 console.log(galleryItems);
-
 
 const galleryItemsContainer = document.querySelector(".gallery");
 const cardsMarcup = createColorCardsMarkup(galleryItems);
 galleryItemsContainer.insertAdjacentHTML("beforeend", cardsMarcup);
 
+galleryItemsContainer.addEventListener("click", onClickGallery);
+
 function createColorCardsMarkup(galleryItems) {
-return galleryItems
+  return galleryItems
     .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
+      return `<div class="gallery__item">
 <a class="gallery__link" href="${original}">
     <img
     class="gallery__image"
@@ -24,26 +25,30 @@ return galleryItems
     .join("");
 }
 
-galleryItemsContainer.addEventListener("click", onClickGallery);
-
-
-
 function onClickGallery(event) {
+  const isGalleryImageEl = event.target.classList.contains("gallery__image");
+  if (!isGalleryImageEl) {
+    return;
+  }
     event.preventDefault();
     
-const isGalleryImageEl = event.target.classList.contains("gallery__image");
-if (!isGalleryImageEl) {
-    return;
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}">
+    `,
+    {
+      onShow: (instance) => {
+        const listener = function (event) {
+          if (event.key === "Escape") {
+            document.removeEventListener("keydown", listener);
+            return instance.close();
+          }
+        };
+        document.addEventListener("keydown", listener);
+      },
+    }
+  );
+
+  instance.show();
 }
-    
-    const largeImgLink = event.target.dataset.source;
-    
-    const instance = basicLightbox.create(`
-<img src="${largeImgLink}" alt="${event.target.alt}">
-`);
-    instance.show();
-}
-
-
-
-
